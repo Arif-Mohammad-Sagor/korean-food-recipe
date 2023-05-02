5d2907be5a76e832c4470c872069ccd30ebf8c93
MdTalha707 <mdsagor0321@gmail.com>
2023-05-02 22:22:45 +0600

    signleChef Details added

--- src/pages/Recipe.jsx
+++ src/pages/Recipe.jsx
@@ -14,7 +14,7 @@ const Recipe = () => {
     fetch(`http://localhost:3000/recipes/${id}`)
       .then((res) => res.json())
       .then((data) => {
-        console.log(data?.recipes)
+
         setRecipes(data?.recipes);
       });
   }, [])
@@ -47,21 +47,30 @@ const Recipe = () => {
           </div>
         </div>
         <h3 className="text-left my-5">My Recipes have a look !!</h3>
-        <div className="row" style={{margin:"mx-auto"}}>
+        <div className="row" style={{ margin: "mx-auto" }}>
           {recipes &&
             recipes.map((recipe) => (
-              <div key={recipe.id} className="col-md-12 col-lg-4  " style={{backgroundColor:"blue",color:"white",margin:'10px',padding:"10px"}}>
+              <div
+                key={recipe.id}
+                className="col-md-12 col-lg-3 "
+                style={{
+                  backgroundColor: "#DBDFEA",
+                  padding: "20px",
+                  margin: "20px",
+                  borderRadius:"20px"
+                }}
+              >
                 <p>
-                  <span className='fw-bold'> RacipeName:</span> {recipe.name}
+                  <span className="fw-bold"> RacipeName:</span> {recipe.name}
                 </p>
 
                 <p>
-                  <span className='fw-bold'>Method: </span>
+                  <span className="fw-bold">Method: </span>
                   <br />
                   {recipe.method}
                 </p>
                 <p>
-                  <span className='fw-bold'> Ingredients:</span>
+                  <span className="fw-bold"> Ingredients:</span>
 
                   <br />
                   {recipe.ingredients &&
